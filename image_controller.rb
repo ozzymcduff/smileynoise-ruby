b912d44b2eb6be1bb4ba3f0eceee89d6576a99c6
Oskar Gewalli <mathieu@Oskar-Gewallis-MacBook-Pro.local>
2010-09-06 06:54:45 +0200

    refactored into a sinatra app.

---
+++ image_controller.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'Image_helper'
+get '/image/view' do
+  @smileyValidation = ImageHelper::SmileyValidation.new()
+  @smileyImage = ImageHelper::SmileyImage.new()
+  smiley = params[:smiley] 
+
+    if (!@smileyValidation.is_valid(smiley))
+      smiley = ':P'
+    end
+    content_type "image/jpeg"
+    
+    smileyimage = @smileyImage.generate(smiley, ! @smileyValidation.is_japanese_style(smiley))
+    smileyimage.to_blob{self.format="jpg"}
+end
--- test/functional/image_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ImageControllerTest < ActionController::TestCase
-  test "should get view" do
-    get :view
-    assert_response :success
-  end
-
-end
