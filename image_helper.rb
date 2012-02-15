f702e3ab3f5c27f0578c84aae8a300ed5a9806f5
Oskar Gewalli <gewalli@gmail.com>
2012-02-15 22:57:32 +0100

    Made sure smiley is recognized.

--- image_helper.rb
+++ image_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'rubygems'
 require 'RMagick' unless defined?(Magick)
 module ImageHelper
--- image_helper_test.rb
+++ image_helper_test.rb
@@ -1,7 +1,8 @@
-require 'test_helper'
+# encoding: utf-8
+require 'image_helper'
 class ImageHelperTest < ActionView::TestCase
   def setup()
-		@smileys = ":-) :) :o) :D :] :3 :c) :> =] 8) =) C: :-D :D 8D XD =D =3 <=3 <=8 <=3 <=8 8===D ( o )( o ) :-( :( :c :< :[ D: D8 D; D= DX v.v :-9 ;-) ;) *) ;] ;D :-P :P XP :-p :p =p :-Þ :Þ :-b :b :-O :O O_O o_o 8O OwO O-O 0_o O_o O3O o0o ;o_o; o...o 0w0 :-/ :/ :\\ =/ =\\ :S :| d:-) qB-) :)~ :-X :X :-# :# O:-) 0:3 O:)  :'( ;*( T_T TT_TT T.T :-* :* >:) >;) B) B-) 8) 8-) ^>.>^ ^<.<^ ^>_>^ ^<_<^ <3 <333 =^_^= =>.>= =<_<= =>.<= \\,,/ \\m/ \\m/\\>.</\\m/ \\o/ \\o o/ o/\\o :& :u @}-;-'--- 8€ (_!_) ^^ oO Oo".split(" ")
+		@smileys = ":-) :) :o) :D :] :3 :c) :> =] 8) =) C: :-D :D 8D XD =D =3 <=3 <=8 <=3 <=8 8===D ( o )( o ) :-( :( :c :< :[ D: D8 D; D= DX v.v :-9 ;-) ;) *) ;] ;D :-P :P XP :-p :p =p :-Þ :Þ :-b :b :-O :O O_O o_o 8O OwO O-O 0_o O_o O3O o0o ;o_o; o...o 0w0 :-/ :/ :\\ =/ =\\ :S :| d:-) qB-) :)~ :-X :X :-# :# O:-) 0:3 O:)  :'( ;*( T_T TT_TT T.T :-* :* >:) >;) B) B-) 8) 8-) ^>.>^ ^<.<^ ^>_>^ ^<_<^ <3 <333 =^_^= =>.>= =<_<= =>.<= \\,,/ \\m/ \\m/\\>.</\\m/ \\o/ \\o o/ o/\\o :& :u @}-;-'--- 8€ (_!_) ^^ oO Oo \"_\" ><".split(" ")
 		@japanese = "O_O o_o OwO O-O 0_o O_o ;o_o; o...o 0w0 T_T TT_TT T.T ^>.>^ ^<.<^ ^>_>^ ^<_<^ =^_^= =>.>= =<_<= =>.<= ^^ oO Oo".split(" ")
 		@val = ImageHelper::SmileyValidation.new()
 	end
